1ddfafc42c87d1f14c26e4ddc57133488ed1570d
unknown <victoria.calderon2502@gmail.com>
2023-06-05 14:48:21 -0600

    Primer Commit

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
-
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 
-
-
 function btnEncriptar() {
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-    mensaje.style.backgroundImage = "none"
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
 }
 
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
 
+  // Verificar si el mensaje está vacío
+  if (stringEncriptada.trim() === "") {
+    alert("Error: El mensaje está vacío.");
+    return null;
+  }
 
-function encriptar(stringEncriptada){
+  // Verificar si contiene caracteres mayúsculas o acentuados
+  const regex = /[A-Záéíóú]/;
+  if (regex.test(stringEncriptada)) {
+    alert("Error: Solo se permiten palabras minúsculas y sin acentos.");
+    return null;
+  }
 
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; 
-    stringEncriptada = stringEncriptada.toLowerCase()
-
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-        }
-
-
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
     }
-    return stringEncriptada
-    
+  }
+
+  return stringEncriptada;
 }
 
 function btnDesencriptar() {
-    const textoEncriptado = desencriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-    
+  const textoEncriptado = desencriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
 
 }
- function btnCopiar(){
-    const textoMensaje= copiar(mensaje.value);
-    document.execCommand('copy');
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.style.backgroundImage = "none";
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+   
+  }
+  
+function btnCopiar() {
+  let mensaje = document.getElementById("mensaje");
+  mensaje.select();
+  mensaje.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
 
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
 
- }
-function desencriptar(stringDesencriptada){
+  if (stringDesencriptada.trim() === "") {
+    alert("Error: El mensaje está vacío.");
+    return null;
+  }
+  const regex = /[A-Záéíóú]/;
+  if (regex.test(stringDesencriptada)) {
+    alert("Error: Solo se permiten palabras minúsculas y sin acentos.");
+    return null;
+  }
 
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; 
-    stringDesencriptada = stringDesencriptada.toLowerCase()
-
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringDesencriptada.includes(matrizCodigo[i][1])){
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
-        }
-
-
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
     }
-    return stringDesencriptada
-    
+  }
+
+  return stringDesencriptada;
 }
